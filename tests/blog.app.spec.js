a8b515a43a263e8983f492850e6647afbb55d0ea
harautia <harautia1976@gmail.com>
2025-10-30 19:32:38 +0200

    Excercise 5.18

--- tests/blog.app.spec.js
+++ tests/blog.app.spec.js
@@ -1,14 +1,26 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Teppo Testaaja',
+        username: 'testaaja',
+        password: 'salainen'
+      }
+    })
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Test User',
+        username: 'tester',
+        password: 'invalid'
+      }
+    })
     await page.goto('http://localhost:3003')
   })
 
-  test('user can log in', async ({ page }) => {
-    await page.getByRole('button', { name: 'login' }).click()
-  })
-/*
 test('front page can be opened', async ({ page }) => {
   await page.goto('http://localhost:3003')
 
@@ -16,5 +28,20 @@ test('front page can be opened', async ({ page }) => {
   await expect(locator).toBeVisible()
   await expect(page.getByText('Blog app , Hannu Rautiainen, 2025')).toBeVisible()
 })
-*/
+
+  test('user can log in', async ({ page }) => {
+    await page.getByRole('button', { name: 'login' }).click()
+    await page.getByLabel('username').fill('testaaja')
+    await page.getByLabel('password').fill('salainen')
+    await page.getByRole('button', { name: 'login' }).click()
+    await expect(page.getByText('Teppo Testaaja logged in')).toBeVisible()
+  })
+
+  test('user unsuccessful log in', async ({ page }) => {
+    await page.getByRole('button', { name: 'login' }).click()
+    await page.getByLabel('username').fill('tester')
+    await page.getByLabel('password').fill('valid')
+    await page.getByRole('button', { name: 'login' }).click()
+    await expect(page.getByText('Wrong Credentials')).toBeVisible()
+  })
 })
